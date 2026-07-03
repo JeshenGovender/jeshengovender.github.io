b7e1e07245efcb766825f22ecef887a113e206c8
parabollica <rahulkap42@gmail.com>
2026-07-03 18:18:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ window.JESHEN_SITE_CONFIG = {
   phoneHref: '',
   instagramHandle: '@tasteandtravelwithjeshen',
   instagramUrl: 'https://instagram.com/tasteandtravelwithjeshen',
-  googleFormEmbedUrl: '',
-  googleFormPublicUrl: ''
+  googleFormEmbedUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSdznj2Ojv0volLoaYDcOH6VzXsxsoqVcDCTzqcEzFZJz0OOgw/viewform?embedded=true',
+  googleFormPublicUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSdznj2Ojv0volLoaYDcOH6VzXsxsoqVcDCTzqcEzFZJz0OOgw/viewform'
 };
